afe4c5d13786d128e4e32fc5f4be77531ca972eb
unknown <176680407@prj31013.ACADEMICOS.uamericas.cl>
2013-10-03 10:05:54 -0300

    Plantilla de uso de API de Facebook

---
+++ src/fb-api/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!-- saved from url=(0067)http://carlosgortaris.cl/udla/ANI321/clases/c3/artifacts/index.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <title>Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="http://carlosgortaris.cl/udla/ANI321/clases/c3/artifacts/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body style="">
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="./index_files/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="./index_files/bootstrap.min.js"></script>
+    <script src="./index_files/funciones.js"></script>
+  
+
+</body></html>
